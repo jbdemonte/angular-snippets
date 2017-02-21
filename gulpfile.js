54cb6cb5ebd33aaf0e7517659275dda784bca3c4
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2017-02-21 07:11:30 +0100

    clean "dist" before build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-var gulp = require("gulp"),
-  uglify = require("gulp-uglify"),
-  rename = require("gulp-rename");
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var clean = require("gulp-clean");
+
+/**
+ * Remove dist directory
+ */
+gulp.task("clean", function () {
+  return gulp.src("dist", {read: false}).pipe(clean());
+});
 
 /**
  * Build non minified version
  */
-gulp.task("main", function() {
+gulp.task("main", ["clean"], function () {
   return gulp.src("src/**/*")
     .pipe(gulp.dest("dist/"));
 });
@@ -13,7 +21,7 @@ gulp.task("main", function() {
 /**
  * Build minified version
  */
-gulp.task("uglify", ["main"], function() {
+gulp.task("uglify", ["main"], function () {
   return gulp.src("dist/**/*.js")
     .pipe(uglify({preserveComments: "some"}))
     .pipe(rename({extname: ".min.js"}))
